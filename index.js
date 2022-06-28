522f3f8eba6268efb87b0b30b2573df75d7501b0
codersimone <97359905+codersimone@users.noreply.github.com>
2022-06-28 05:21:41 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-function changeTheme() {
-    if (document.getElementById("theme").value === "0")
-        document.body.className = "light";
-    if (document.getElementById("theme").value === "1")
-        document.body.className = "dark";
-    if (document.getElementById("theme").value === "2")
-        document.body.className = "blue";
-}
